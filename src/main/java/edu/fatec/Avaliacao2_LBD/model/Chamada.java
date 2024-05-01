716a603e7cdd0b633c167a166602d704c57ba442
Luan <luancamilo313@gmail.com>
2024-04-30 23:07:55 -0300

    Objeto Chamada Criado
    Script para a criação das tabelas e seus determinados dados acionados na pasta sql

---
+++ src/main/java/edu/fatec/Avaliacao2_LBD/model/Chamada.java
@@ -0,0 +1,16 @@
+package edu.fatec.Avaliacao2_LBD.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chamada {
+    private int id;
+    private int presenca;
+    private int aula;
+}
