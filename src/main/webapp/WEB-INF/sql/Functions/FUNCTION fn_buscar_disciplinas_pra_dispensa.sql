31373698e843a454787e89bf18be1b6c769fe103
DeboraCristina <deboracristinaproficional1@gmail.com>
2024-05-10 21:41:58 -0300

    CORREÇÃO: busca por disciplinas disponiveis para dispensa retorna os dados necessários

--- src/main/webapp/WEB-INF/sql/Functions/FUNCTION fn_buscar_disciplinas_pra_dispensa.sql
+++ src/main/webapp/WEB-INF/sql/Functions/FUNCTION fn_buscar_disciplinas_pra_dispensa.sql
@@ -6,9 +6,14 @@ GO
     DROP FUNCTION fn_buscar_disciplinas_pra_dispensa
 */
 
+DROP FUNCTION fn_buscar_disciplinas_pra_dispensa
+
+GO
+
 CREATE FUNCTION fn_buscar_disciplinas_pra_dispensa(@ra VARCHAR(50))
 RETURNS @table TABLE
 (
+    id_matricula INT,
     codigo INT,
     nome_disc VARCHAR(100),
     nome_professor VARCHAR(100),
@@ -20,25 +25,33 @@ BEGIN
 
     SELECT @curso=c.codigo FROM curso c, matricula m WHERE m.cod_curso = c.codigo AND m.ra = @ra
 
-    INSERT INTO @table (codigo, nome_disc, nome_professor, num_aulas, situacao)
-        SELECT d.codigo AS codigo_disciplina, d.nome AS nome_disciplina, prof.nome AS nome_professor,
-        d.horas_semanais,
-        CASE WHEN (md.estado IS NULL ) THEN 'DISPONIVEL' ELSE md.estado END AS situacao
-        FROM disciplina d LEFT JOIN matricula_disciplina md ON md.cod_disciplina = d.codigo
-        , professor prof, curso_disciplina c
-        WHERE c.cod_curso = @curso AND c.cod_disciplina = d.codigo AND
-        (md.ra_matricula = @ra OR md.ra_matricula IS NULL ) AND
-        (md.estado LIKE 'REPROVADO' OR md.estado IS NULL )
-        AND d.id_professor = prof.id
-        AND d.codigo NOT IN (
-            SELECT codigo FROM fn_disciplinas_matriculadas(@ra)
-        )
-        AND d.codigo NOT IN (
-            SELECT d.codigo AS cod
-            FROM matricula_disciplina md, disciplina d, horario h
-            WHERE md.cod_disciplina = d.codigo AND md.id_horario = h.id AND
-                ra_matricula = '202321485' AND estado LIKE 'aprovado'
-        )
+    INSERT INTO @table (id_matricula, codigo, nome_disc, nome_professor, num_aulas, situacao)
+        SELECT id_matricula_disc, codigo_disciplina, nome_disciplina, nome_professor, horas_semanais, situacao
+        FROM (
+            SELECT
+                CASE WHEN (md.id IS NULL OR md.id = 0) THEN 0 ELSE md.id END AS id_matricula_disc,
+                c.cod_disciplina AS codigo_disciplina,
+                d.nome AS nome_disciplina,
+                prof.nome AS nome_professor,
+                d.horas_semanais,
+                CASE WHEN (md.estado IS NULL) THEN 'DISPONIVEL' ELSE UPPER(md.estado) END AS situacao,
+                ROW_NUMBER() OVER(PARTITION BY c.cod_disciplina ORDER BY CASE WHEN md.estado = 'MATRICULADO' THEN 0 ELSE 1 END) AS rn
+
+            FROM curso_disciplina c
+            LEFT JOIN matricula_disciplina md ON md.cod_disciplina = c.cod_disciplina AND md.ra_matricula = @ra
+            JOIN disciplina d ON c.cod_disciplina = d.codigo
+            JOIN professor prof ON d.id_professor = prof.id
+
+            WHERE c.cod_curso = @curso
+            AND (md.estado != 'DISPENSADO' OR md.estado IS NULL)
+            AND d.codigo NOT IN (
+                SELECT d.codigo AS cod
+                FROM matricula_disciplina md, disciplina d, horario h
+                WHERE md.cod_disciplina = d.codigo AND md.id_horario = h.id AND
+                    ra_matricula = @ra AND estado LIKE 'aprovado'
+            )
+        ) AS temp
+        WHERE rn = 1;
 
     RETURN
 
